ba3c17a68af066f2667febf936d7dc364a71d9ee
cat-bro <cathjb@gmail.com>
2020-01-29 14:35:57 +1100

    correct some mistakes

--- scripts/organise_request_files.py
+++ scripts/organise_request_files.py
@@ -3,9 +3,7 @@ import argparse
 
 
 def main():
-    VERSION = 'development'
-
-    parser = argparse.ArgumentParser(description='Rewrite arbitrarily many tool yml files as one file per tool revision')
+    parser = argparse.ArgumentParser(description='Rewrite arbitrarily many tool.yml files as one file per tool revision')
     parser.add_argument('-o', '--output_path', help='Output file path')
     parser.add_argument('-f', '--files', help='Tool input files', nargs='+')
 
@@ -25,13 +23,13 @@ def main():
 
     for tool in tools_by_entry:
         if 'revisions' in tool.keys() and len(tool['revisions']) > 1:
-            tool_revisions = []
             for rev in tool['revisions']:
                 new_tool = tool
                 new_tool['revisions'] = [rev]
                 write_output_file(path=path, tool=new_tool)
         else:
             write_output_file(path=path, tool=tool)
+
 
 def write_output_file(path, tool):
     if not path[-1] == '/':
@@ -42,4 +40,6 @@ def write_output_file(path, tool):
     with open(file_path, 'w') as outfile:
         outfile.write(yaml.dump({'tools': [tool]}))
 
-if __name__ == "__main__": main()
+
+if __name__ == "__main__":
+    main()
