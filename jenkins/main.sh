3361330b3087440c34533ac9a7e1a0ffd39634eb
cat-bro <cathjb@gmail.com>
2020-01-31 16:26:04 +1100

    bring in cron job support

--- jenkins/main.sh
+++ jenkins/main.sh
@@ -1,9 +1,15 @@
 #! /bin/bash
-chmod +x jenkins/webhook_install_tools.sh
+chmod +x jenkins/install_tools.sh
 
-INSTALL_ID="$(date '+%Y%m%d%H%M%S')" # this will do for now, could incorporate jenkins build ID or git commit hash
+MODE="$1"; # Two modes possible: "REQUEST" for tool request, "UPDATE" for cron update
+FILE_ARGS=("${array[@]:1}")
 LOG_DIR=~/galaxy_tool_automation
 BASH_V="$(echo ${BASH_VERSION} | head -c 1)" # this will be "4" if the bash version is 4.x, empty otherwise
+
+if [ ! "$MODE" = "REQUEST" ] && [ ! "$MODE" = "UPDATE" ]; then
+  echo "First positional argument to jenkins/main must be REQUEST or UPDATE"
+  exit 1
+fi
 
 # Use RUN_SCRIPT_ON_JENKINS variable as a switch to allow the script to be run
 # locally or remotely at stages of development. If RUN_SCRIPT_ON_JENKINS is 0,
@@ -17,6 +23,7 @@ export LOCAL_ENV=$LOCAL_ENV
 if [ $LOCAL_ENV = 1 ]; then
     # GIT_COMMIT and GIT_PREVIOUS_COMMIT are supplied by Jenkins
     # Use HEAD and HEAD~1 when running locally
+    BUILD_NUMBER="local_$(date '+%Y%m%d%H%M%S')"
     GIT_PREVIOUS_COMMIT=HEAD~1;
     GIT_COMMIT=HEAD;
     LOG_DIR="logs"
@@ -30,52 +37,54 @@ fi
 
 [ -d $LOG_DIR ] || mkdir $LOG_DIR;
 
-export LOG_FILE="$LOG_DIR/webhook_tool_installation_$INSTALL_ID"
+export BUILD_NUMBER=$BUILD_NUMBER
+export LOG_FILE="$LOG_DIR/webhook_tool_installation_$BUILD_NUMBER"
 export GIT_COMMIT=$GIT_COMMIT
 export GIT_PREVIOUS_COMMIT=$GIT_PREVIOUS_COMMIT
-export BUILD_NUMBER=$BUILD_NUMBER
-export INSTALL_ID=$INSTALL_ID
 export LOG_DIR=$LOG_DIR
 export BASH_V=$BASH_V
+export MODE=$MODE
 
 jenkins_tool_installation() {
-  # First check whether changed files are in the path of tool requests, that is within the requests folder but not within
-  # any subfolders of requests.  If so, we run the install script.  If not we exit.
-  REQUESTS_DIFF=$(git diff --name-only --diff-filter=A $GIT_PREVIOUS_COMMIT $GIT_COMMIT | cat | grep "^requests\/[^\/]*$")
+  if [ $MODE = "REQUEST" ]; then
+    # First check whether changed files are in the path of tool requests, that is within the requests folder but not within
+    # any subfolders of requests.  If so, we run the install script.  If not we exit.
+    REQUESTS_DIFF=$(git diff --name-only --diff-filter=A $GIT_PREVIOUS_COMMIT $GIT_COMMIT | cat | grep "^requests\/[^\/]*$")
 
-  # Arrange git diff into string "file1 file2 .. fileN"
-  REQUEST_FILES=$REQUESTS_DIFF
-  if [[ "$REQUESTS_DIFF" == *$'\n'* ]]; then
-    REQUEST_FILES=$(echo $REQUESTS_DIFF | tr "\n" " ")
-  fi
+    # Arrange git diff into string "file1 file2 .. fileN"
+    REQUEST_FILES=$REQUESTS_DIFF
+    if [[ "$REQUESTS_DIFF" == *$'\n'* ]]; then
+      REQUEST_FILES=$(echo $REQUESTS_DIFF | tr "\n" " ")
+    fi
 
-  if [ $LOCAL_ENV = 1 ] && [[ "$*" ]]; then # if running locally, allow a filename argument
-    echo Running locally, installing "$*";
-    REQUEST_FILES="$*";
-  fi
+    if [ $LOCAL_ENV = 1 ] && [[ "$*" ]]; then # if running locally, allow a filename argument
+      echo Running locally, installing "$*";
+      REQUEST_FILES="$*";
+    fi
 
-  export REQUEST_FILES=$REQUEST_FILES
+    export REQUEST_FILES=$REQUEST_FILES
 
-  if [[ ! $REQUEST_FILES ]]; then
-    # Exit early and do not write a log if the commit does not contain request files
-    echo "No added files in requests folder, no tool installation required";
-    exit 0;
-  else
-    echo "Tools from the following files will be installed";
-    echo $REQUEST_FILES;
+    if [[ ! $REQUEST_FILES ]]; then
+      # Exit early and do not write a log if the commit does not contain request files
+      echo "No added files in requests folder, no tool installation required";
+      exit 0;
+    else
+      echo "Tools from the following files will be installed";
+      echo $REQUEST_FILES;
+    fi
   fi
 
   echo "Saving output to $LOG_FILE"
   if [ $LOCAL_ENV = 0 ]; then
-    bash jenkins/webhook_install_tools.sh &> $LOG_FILE
+    bash jenkins/install_tools.sh &> $LOG_FILE
     cat $LOG_FILE
   else
     # Do not save a log file when running locally
-    bash jenkins/webhook_install_tools.sh
+    bash jenkins/install_tools.sh
   fi
 }
 
 # Always run locally, if running on Jenkins run only when switched on (1)
 if [ $LOCAL_ENV = 1 ] || [ $RUN_SCRIPT_ON_JENKINS = 1 ]; then
-  jenkins_tool_installation "$@"
+  jenkins_tool_installation "${FILE_ARGS[@]}"
 fi
--- scripts/organise_request_files.py
+++ scripts/organise_request_files.py
@@ -1,16 +1,35 @@
 import yaml
 import argparse
+import sys
+import os
 
+trusted_owners = ['iuc']
 
 def main():
     parser = argparse.ArgumentParser(description='Rewrite arbitrarily many tool.yml files as one file per tool revision')
-    parser.add_argument('-o', '--output_path', help='Output file path')
-    parser.add_argument('-f', '--files', help='Tool input files', nargs='+')
+    parser.add_argument('-o', '--output_path', help='Output file path')  # mandatory
+    parser.add_argument('-f', '--files', help='Tool input files', nargs='+')  # mandatory unless --update_existing is true
+    parser.add_argument(
+        '--update_existing',
+        help='If there are several toolshed entries for one name or name/revision entry uninstall all of them',
+        action='store_true',
+    )
+    parser.add_argument('-s', '--source_directory', help='Directory containing tool yml files')
 
     args = parser.parse_args()
 
     files = args.files
     path = args.output_path
+    update = args.update_existing
+    source_dir = args.source_directory
+
+    if not (files or source_dir):
+        sys.stderr.write('either --files or --source_directory must be defined as an argument\n')
+        return
+    elif files and source_dir:
+        sys.stderr.write('--files and --source_directory have both been provided.  Ignoring source_directory in favour of files\n')
+    if source_dir and not files:
+        files = os.listdir(source_dir)
 
     tools_by_entry = []
     for file in files:
@@ -20,6 +39,13 @@ def main():
                 tools_by_entry += content
             else:
                 tools_by_entry.append(content)
+
+    if update:  # only update tools with trusted owners
+        tools_by_entry = [t for t in tools_by_entry if t['owner'] in trusted_owners]
+        for tool in tools_by_entry:
+            for key in tool.keys():  # delete extraneous keys, we want latest revision
+                if key not in ['name', 'owner', 'tool_panel_section_label', 'tool_shed_url']:
+                    delete tool[key]
 
     for tool in tools_by_entry:
         if 'revisions' in tool.keys() and len(tool['revisions']) > 1:
