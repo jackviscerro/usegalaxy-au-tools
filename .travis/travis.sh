f5a0454dcf2834638ac83dba4390d68071521ede
cat-bro <catherine.bromhead@unimelb.edu.au>
2020-08-24 15:46:39 +1000

    .

--- .travis/travis.sh
+++ .travis/travis.sh
@@ -2,24 +2,26 @@
 
 # This script will run for a pull request or will run locally if the argument
 # 'local' is provided
-if [ ! $TRAVIS_PULL_REQUEST ] && [ ! "$@" = "local" ]; then
-  exit 0;
-fi
+# if [ ! $PULL_REQUEST_HEAD ] && [ ! "$@" = "local" ]; then
+#   exit 0;
+# fi
 
 source .env  # Load non-secret enviroment variables e.g. $STAGING_URL
 
-if [[ "$@" = "local" ]]; then
-  TRAVIS_BRANCH=master
-fi
-
-echo "TRAVIS_BRANCH: $TRAVIS_BRANCH";
-echo "TRAVIS_PULL_REQUEST_BRANCH: $TRAVIS_PULL_REQUEST_BRANCH";
+# if [[ "$@" = "local" ]]; then
+#   TRAVIS_BRANCH=master
+# fi
+# 
+# echo "TRAVIS_BRANCH: $TRAVIS_BRANCH";
+# echo "TRAVIS_PULL_REQUEST_BRANCH: $TRAVIS_PULL_REQUEST_BRANCH";
 
 # Added files in requests directory
-REQUEST_FILES=$(git diff --diff-filter=A --name-only $TRAVIS_BRANCH | cat | grep "^requests\/[^\/]*$")
+REQUEST_FILES=$(git diff --diff-filter=A --name-only | cat | grep "^requests\/[^\/]*$")
+# REQUEST_FILES=$(git diff --diff-filter=A --name-only $TRAVIS_BRANCH | cat | grep "^requests\/[^\/]*$")
 
 # Altered files in tool directories (eg. galaxy-aust-staging) written to by jenkins
-JENKINS_CONTROLLED_FILES=$(git diff --name-only $TRAVIS_BRANCH | cat  | grep "^(?:\$STAGING_TOOL_DIR\/|PRODUCTION_TOOL_DIR\/).*/")
+JENKINS_CONTROLLED_FILES=$(git diff --name-only | cat  | grep "^(?:\$STAGING_TOOL_DIR\/|PRODUCTION_TOOL_DIR\/).*/")
+# JENKINS_CONTROLLED_FILES=$(git diff --name-only $TRAVIS_BRANCH | cat  | grep "^(?:\$STAGING_TOOL_DIR\/|PRODUCTION_TOOL_DIR\/).*/")
 
 if [ $JENKINS_CONTROLLED_FILES ]; then
   echo "Files within $PRODUCTION_TOOL_DIR or $STAGING_TOOL_DIR are written by Jenkins and cannot be altered";
